2878f8a7e50754028423be24c21da7517db2d93a
LShapir0 <lshapir6@terpmail.umd.edu>
2023-12-12 14:50:23 -0500

    Most amount of Fibonacci numbers I figured out how to generate without causing overflow.

--- main.c
+++ main.c
@@ -1,21 +1,15 @@
 #include <stdio.h>
 
 int main() {
-    int i;
-    int sum = 0;
-    int tmp[10];
+   long  int i;
+   long  int tmp[500];
 
     tmp[0] = 1;
     tmp[1] = 2;
-    for(i = 2; i<9; i++){
+    for(i = 2; i<91; i++){// i < (Fibonacci number - 1)
         tmp[i] = tmp[i-2] + tmp[i-1];
-        printf("%d\t", tmp[i]);
-        if(tmp[i] % 2 == 0){
-            sum = sum + tmp[i];
-        }
+        printf("%ld\t\t%ld\n", tmp[i], i + 2);
     }
-    printf("\n%d",sum + 2);
-
     return 0;
 }
 
