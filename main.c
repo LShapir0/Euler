94661ec8a143b8a0451592714d63114e06e4feff
Daniel Henneberger <git@danielhenneberger.com>
2023-12-12 12:17:42 -0800

    Add more concise function
    Signed-off-by: Daniel Henneberger <git@danielhenneberger.com>

--- main.c
+++ main.c
@@ -1,17 +1,22 @@
 #include <stdio.h>
 
+long sumFib(int i);
+
 int main() {
-   long  int i;
-   long  int tmp[500];
-   long int sum = 0;
-    tmp[0] = 1;
-    tmp[1] = 2;
-    for(i = 2; i<91; i++) {// i < (Fibonacci number - 1)
-        tmp[i] = tmp[i - 2] + tmp[i - 1];
-            if(tmp[i] % 2 == 0 && tmp[i] < 4000000) {
-                sum = sum + tmp[i];
+    printf("Sum: %ld", sumFib(4000000));
+    return 0;
+}
+
+long sumFib(int limit) {
+    long sum = 0;
+    int last = 1, cur = 1;
+    while(cur < limit) {
+        int tmp = cur;
+        cur = last + cur;
+        last = tmp;
+        if (cur % 2 == 0) {
+            sum = sum + cur;
         }
     }
-    printf("Sum: %ld", sum + 2);
-    return 0;
+    return sum;
 }
