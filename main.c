9462918ebd0cf5283d1e0f9696dd2b8904f2bbea
LShapir0 <lshapir6@terpmail.umd.edu>
2023-12-12 15:01:45 -0500

    Solution.

--- main.c
+++ main.c
@@ -3,13 +3,15 @@
 int main() {
    long  int i;
    long  int tmp[500];
-
+   long int sum = 0;
     tmp[0] = 1;
     tmp[1] = 2;
-    for(i = 2; i<91; i++){// i < (Fibonacci number - 1)
-        tmp[i] = tmp[i-2] + tmp[i-1];
-        printf("%ld\t\t%ld\n", tmp[i], i + 2);
+    for(i = 2; i<91; i++) {// i < (Fibonacci number - 1)
+        tmp[i] = tmp[i - 2] + tmp[i - 1];
+            if(tmp[i] % 2 == 0 && tmp[i] < 4000000) {
+                sum = sum + tmp[i];
+        }
     }
+    printf("Sum: %ld", sum + 2);
     return 0;
 }
-
